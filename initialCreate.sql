87e333dea9ca42f7fc402b3c5aaeb660c4cc6789
Jeff <j.michael.press@gmail.com>
2022-07-26 15:53:20 -0700

    First commit: openAPI specification and PostgreSQL table create statements

---
+++ initialCreate.sql
@@ -0,0 +1,15 @@
+CREATE TABLE envelopes (
+    envelope_id integer PRIMARY KEY,
+    envelope_name varchar(14) UNIQUE NOT NULL,
+    current_value float,
+    budgeted_value float,
+    isIncome boolean
+);
+
+CREATE TABLE transactions (
+    transaction_id integer PRIMARY KEY,
+    wd_envelope_id integer REFERENCES envelopes(envelope_id),
+    transaction_date date,
+    payment_recipient varchar(14),
+    payment_amount float
+);
