91a9690518b59b6580268363012db9673ae10c9b
Jeff <j.michael.press@gmail.com>
2022-08-02 14:16:18 -0700

    heroku db access string

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const connectionString = 'postgres://nrzepyzgnlthqb:a92b8ef5c8c24bb819383751f8ec0150fe9d44ceb92254e4fe6124c060a9d25c@ec2-54-208-104-27.compute-1.amazonaws.com:5432/dijm0q1volemf'
+
 const pool = new Pool({
+    connectionString: connectionString,
     /*
     user: process.env.PMAPIUSER,
     host: process.env.PMHOST,
